b7de25a8f8f221bdc99ce449ad4a80d561781c40
tienxuanpt <tienxuanpt@gmail.com>
2021-11-03 18:52:02 +0700

    add

--- config.js
+++ config.js
@@ -3,7 +3,8 @@ var common = {};
 
 // Cấu hình js
 common.config = {
-    urlBinance: "https://api.binance.com/api/v3/ticker/price?symbol={0}USDT",
+    //urlBinance: "https://api.binance.com/api/v3/ticker/price?symbol={0}USDT",
+    urlBinance: "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR&api_key=24e6b21185b3f3a399453aa2bb0203b326956732e9631c00a0a7400f4907ef10",
     priceVN: 23500
 };
 
@@ -14,7 +15,8 @@ common.Ajax = (url, method, fnCallBack, async = true) => {
         method: method,
         async: async,
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            "authorization": "Apikey 24e6b21185b3f3a399453aa2bb0203b326956732e9631c00a0a7400f4907ef10"
         },
         crossDomain: true,
         contentType: "application/json;charset=utf-8",
