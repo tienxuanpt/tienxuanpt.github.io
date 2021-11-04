c2649c07fe9ce85daa121a709a3c58d0b944d06e
tienxuanpt <tienxuanpt@gmail.com>
2021-11-04 08:03:22 +0700

    add

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ class Binance {
         me.intervalID = null;
         // Thời gian lặp lại biểu đồ
         me.interValTime = 15;
+        // Gía đô
+        me.priceVN = 23500;
 
         // Khởi tạo sự kiện
         me.initEvents();
@@ -75,6 +77,8 @@ class Binance {
         me.interValTime = Number.parseInt($(".time-refresh").val());
         // Số tiền chốt lời
         me.amountStop = Number.parseInt($(".amount-stop").val());
+        // Gía đô
+        me.priceVN = Number.parseInt($(".amount-VN").val());
 
         // Xóa interval hiện tại
         if(me.intervalID){
@@ -113,13 +117,13 @@ class Binance {
         if (data && data.length > 0) {
             data.filter(function (item) {
                 // Số tiền cũ
-                item.amount_old = Math.round(Number.parseFloat(item.price_old) * common.config.priceVN * Number.parseFloat(item.quantity));
+                item.amount_old = Math.round(Number.parseFloat(item.price_old) * me.priceVN * Number.parseFloat(item.quantity));
 
                 // Lấy giá hiện tại của coin
                 item.price_current = me.getPriceCurrent(item);
 
                 // Tính số tiền mới
-                item.amount_current = Math.round(item.price_current * common.config.priceVN * Number.parseFloat(item.quantity));
+                item.amount_current = Math.round(item.price_current * me.priceVN * Number.parseFloat(item.quantity));
 
                 // Tính số tiền chênh lệch
                 item.amount_diff = item.amount_current - item.amount_old;
